0048396ead6842583d5664664ca4a5cc445cf20d
Echron <stijn@echron.be>
2017-04-21 00:23:17 +0200

    Change execution of commands implementation so it handles registered commands + update packages

--- worker/run.php
+++ worker/run.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-define('BP', dirname(__FILE__));
+define('BP_WORKER', dirname(__FILE__));
 
-require BP . '/../src/vendor/autoload.php';
+require BP_WORKER . '/../src/vendor/autoload.php';
 
 $logger = new \Attlaz\Framework\App\Logger('Attlaz');
 $webhookUrl = 'https://hooks.slack.com/services/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';
@@ -27,6 +27,38 @@ $logger->pushHandler($streamHandler);
 
 \Monolog\ErrorHandler::register($logger);
 
-$settings = \Attlaz\Framework\Model\Settings::fromFile(BP . '/../config.yml');
-$app = new \Attlaz\Worker\Worker($settings, $logger);
-$app->listen();
+$projectRegistrationFile = BP_WORKER . '/../project/src/registration.php';
+if (!file_exists($projectRegistrationFile)) {
+    $logger->error('Unable to load project registration file, file does not exist');
+} else {
+    require BP_WORKER . '/../project/src/registration.php';
+
+    $project = \Attlaz\Framework\App\ProjectRegistrar::getProject();
+    $settings = \Attlaz\Framework\Model\Settings::fromFile(BP_WORKER . '/../config.yml');
+
+    /** @var \Psr\Log\LoggerInterface $projectLogger */
+    $projectLogger = $project->getContainer()
+                             ->get(\Psr\Log\LoggerInterface::class);
+    if ($projectLogger instanceof \Monolog\Logger || $projectLogger instanceof Attlaz\Framework\App\Logger) {
+        $projectLogger->pushHandler($slackHandler);
+        $projectLogger->pushHandler($streamHandler);
+    }
+
+    $executeTaskHelper = new \Attlaz\Worker\Helper\ExecuteTaskHelper($project, $projectLogger);
+
+    if (false) {
+        $task = new \Attlaz\Framework\Model\Task('syncCatalog', [
+            'externalIds' => [
+                11505,
+                15706,
+                14757,
+                8793,
+            ],
+        ]);
+        $executeTaskHelper->__invoke($task);
+
+        return;
+    }
+    $app = new \Attlaz\Worker\Worker($settings, $executeTaskHelper, $projectLogger);
+    $app->listen();
+}
