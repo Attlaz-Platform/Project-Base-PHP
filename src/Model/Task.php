aa16bb147158e9832509deb5dd463cf493cc55b8
Echron <stijn@echron.be>
2018-02-21 00:45:41 +0100

    Task fromArray: don't throw error when arguments are not defined

--- src/Model/Task.php
+++ src/Model/Task.php
@@ -45,15 +45,15 @@ class Task implements \JsonSerializable
 
     public static function fromArray(array $input): self
     {
-        if (!\key_exists('arguments', $input)) {
-            throw new \InvalidArgumentException('Unable to deserialize Task, arguments is not defined');
-        }
         if (!\key_exists('command', $input)) {
             throw new \InvalidArgumentException('Unable to deserialize Task, command is not defined');
         }
-
         $command = $input['command'];
-        $arguments = $input['arguments'];
+
+        $arguments = [];
+        if (\key_exists('arguments', $input)) {
+            $arguments = $input['arguments'];
+        }
 
         return new self($command, $arguments);
     }
