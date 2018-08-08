bc6a99f208874f9672ef516ccc333c52019c1d37
Stijn Duynslaeger <stijn@echron.be>
2018-08-08 15:12:48 +0200

    Logging: use API to log instead of direct MongoDB

--- src/Project.php
+++ src/Project.php
@@ -3,10 +3,10 @@ declare(strict_types=1);
 
 namespace Attlaz\Project;
 
+use Attlaz\Project\App\Config;
 use Attlaz\Project\Helper\ExecuteTaskHelper;
 use Attlaz\Project\Helper\TaskExecutionRequestHelper;
 use Attlaz\Project\Model\Log\Processor;
-use Attlaz\Project\Model\ProjectConfig;
 use Attlaz\Project\Model\TaskExecutionRequest;
 use Attlaz\Project\Model\TaskExecutionResult;
 use Attlaz\Project\Serialization\SerializeTaskResult;
@@ -16,7 +16,6 @@ use Psr\Log\LoggerInterface;
 
 class Project
 {
-    private $branchCode;
 
     private $commands;
 
@@ -26,45 +25,34 @@ class Project
     /** @var LoggerInterface */
     private $logger;
 
-    private $mode = self::MODE_PRODUCTION;
-
-    public const MODE_PRODUCTION = 'production';
-    public const MODE_DEVELOP = 'develop';
     private $startTime;
 
+    /** @var Config */
     private $config;
 
-    public function __construct(ProjectConfig $config)
+    public function __construct(Config $config = null)
     {
         $this->startTime = \microtime(true);
 
+        if (\is_null($config)) {
+            $config = new Config();
+        }
         $this->config = $config;
 
         ini_set('memory_limit', '2G');
         date_default_timezone_set('Europe/Brussels');
 
-        $this->branchCode = $config->branchCode;
-        $this->commands = [];
+        if ($this->config->mode === Config::MODE_DEVELOPMENT) {
+            error_reporting(E_ALL);
+            ini_set('display_errors', '1');
+        }
 
-        $this->setMode($config->mode);
+        $this->commands = [];
 
         $this->initDI($config->definitionsFile);
 
         $this->logger = $this->getContainer()
                              ->get(LoggerInterface::class);
-    }
-
-    public function setMode(string $mode)
-    {
-        if ($mode !== self::MODE_PRODUCTION && $mode !== self::MODE_DEVELOP) {
-            throw new \InvalidArgumentException('Invalid mode "' . $mode . '", must be product or develop');
-        }
-        $this->mode = $mode;
-
-        if ($this->mode === self::MODE_DEVELOP) {
-            error_reporting(E_ALL);
-            ini_set('display_errors', '1');
-        }
     }
 
     private function initDI(string $definitionsFile = null)
@@ -78,23 +66,15 @@ class Project
         /** @var \DI\ContainerBuilder $containerBuilder */
         $containerBuilder = new ContainerBuilder();
 
-        if ($this->mode === self::MODE_PRODUCTION) {
-        $containerBuilder->enableCompilation(__DIR__ . '/var/cache');
+        if ($this->config->mode === Config::MODE_PRODUCTION) {
+            $containerBuilder->enableCompilation(BP . '/var/cache');
+            // TODO: this doesn't make sense with PHP CLI
+            //$containerBuilder->enableDefinitionCache();
         }
-        $containerBuilder->addDefinitions(['branchCode' => $this->branchCode]);
 
-        $mongoDBConnectionString = 'mongodb://mongo-admin:UCGJbmQ25Kdx@storage:27017';
-
-        $mongoDBUriOptions = [
-            'readPreference' => 'nearest',
-        ];
-
-        $containerBuilder->addDefinitions(['mongoDBConnectionString' => $mongoDBConnectionString]);
-        $containerBuilder->addDefinitions(['mongoDBUriOptions' => $mongoDBUriOptions]);
+        $containerBuilder->addDefinitions(['config' => $this->config]);
 
         $containerBuilder->addDefinitions(__DIR__ . '/di.php');
-//        $containerBuilder->addDefinitions($this->getDefinitions());
-
         if (!\is_null($definitionsFile)) {
             $containerBuilder->addDefinitions($definitionsFile);
         }
@@ -140,8 +120,6 @@ class Project
 
     public function handleRequest(TaskExecutionRequest $request = null): void
     {
-        echo 'Start handle request: ' . number_format(\microtime(true) - $this->startTime, 4) . ' sec.' . \PHP_EOL;
-
         try {
             if (\is_null($request)) {
                 $request = TaskExecutionRequestHelper::getRequest();
@@ -158,7 +136,7 @@ class Project
             $strTaskResult = $cmd->__invoke($taskExecutionResult);
 
             $this->logger->debug('Sending back response: ' . $strTaskResult);
-            echo 'Complete: ' . number_format(\microtime(true) - $this->startTime, 4) . ' sec.' . \PHP_EOL;
+
             $this->sendResponse($strTaskResult);
             if ($taskExecutionResult->getSuccess()) {
                 exit(0);
@@ -170,10 +148,6 @@ class Project
             $this->logger->error($ex->getMessage());
             exit(1);
         }
-
-        // ob_end_flush();
-
-//        exit(1);
     }
 
     private function sendResponse(string $result)
@@ -188,14 +162,4 @@ class Project
         return $cmd->__invoke($task);
     }
 
-//    private function runAsLocal(): bool
-//    {
-//        $jetbrains = \getenv('JETBRAINS_REMOTE_RUN');
-//        if ($jetbrains === '1') {
-//            return true;
-//        }
-//
-//        //TODO: handle local test run in CLI
-//        return false;
-//    }
 }
