57a03028968e7cfe522ab82550567f52ba663d68
Stijn Duynslaeger <stijn@echron.be>
2018-11-09 22:25:03 +0100

    ATL-158 + ATL-157: Add interactive CLI command to execute task + fix task execution exit code

---
+++ src/Cli/Command/ExecuteTaskInteractive.php
@@ -0,0 +1,99 @@
+<?php
+declare(strict_types=1);
+
+namespace Attlaz\Project\Cli\Command;
+
+use Attlaz\Project\Model\TaskExecutionRequest;
+use Attlaz\Project\Model\TaskExecutionResult;
+use Attlaz\Project\Serialization\SerializeTaskResult;
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\Question\Question;
+
+class ExecuteTaskInteractive extends ExecuteTask
+{
+   
+    protected function configure()
+    {
+        $this->setName('task:execute:interactive')
+             ->setDescription('Run task.')
+             ->setHelp('This command allows you to run a task interactively');
+        //     ->addArgument('task', InputArgument::REQUIRED, 'Task (id) to execute')
+//             ->addOption('arguments', null, InputOption::VALUE_REQUIRED, 'How many times should the message be printed?', null)
+//             ->addOption('execution', null, InputOption::VALUE_REQUIRED, 'How many times should the message be printed?', 'some random string');
+
+    }
+
+    /** @noinspection PhpMissingParentCallCommonInspection */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        try {
+            $commands = $this->commandManager->getCommandDefinitions();
+
+            $taskIds = [];
+            foreach ($commands as $command) {
+                $taskIds[$command->task] = '' . $command->className . '';
+            }
+
+            $questionHelper = new QuestionHelper();
+
+            /**
+             * Request taskId
+             */
+            $question = new ChoiceQuestion('Please select a task:', $taskIds);
+            $question->setErrorMessage('Task %s is invalid.');
+
+            $taskId = $questionHelper->ask($input, $output, $question);
+
+            $output->writeln('You have just selected: ' . $taskId);
+
+            $selectedCommand = null;
+            foreach ($commands as $command) {
+                if ($taskId === $command->task) {
+                    $selectedCommand = $command;
+                }
+            }
+            /**
+             * Request parameters
+             */
+
+            $parameterValues = [];
+            $parameters = $selectedCommand->getParameters();
+
+            foreach ($parameters as $parameter) {
+                $parameterString = '\'' . $parameter->getName() . '\'';
+                if ($parameter->hasType()) {
+                    $parameterString .= ' (' . $parameter->getType() . ')';
+                } else {
+                    $parameterString .= ' (No type specified)';
+                }
+
+                if ($parameter->isRequired()) {
+                    $parameterString .= ' [required]';
+                } else {
+                    $parameterString .= ' [default: ' . $parameter->getDefault() . ']';
+                }
+
+                $question = new Question('Please enter a value for parameter ' . $parameterString . ':', $parameter->getDefault());
+
+                $parameterValue = $questionHelper->ask($input, $output, $question);
+//TODO: validate input
+                $output->writeln('You have just selected: ' . $parameterValue);
+
+                $parameterValues[$parameter->getName()] = $parameterValue;
+            }
+
+            $taskExecutionRequest = new TaskExecutionRequest($taskId, $parameterValues, 'soe');
+
+            return $this->executeTaskExecutionRequest($taskExecutionRequest);
+        } catch (\Throwable $ex) {
+            $this->logger->error($ex->getMessage());
+
+            return 1;
+        }
+    }
+
+  
+}
