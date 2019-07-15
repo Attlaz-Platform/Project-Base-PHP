fdbd65dbd7dbfec02e88c5e4d1f9784998865854
Stijn Duynslaeger <stijn@echron.be>
2019-07-15 19:20:21 +0200

    When running in local environment update the task execution status

--- src/TaskExecution/AbstractTaskHandler.php
+++ src/TaskExecution/AbstractTaskHandler.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Attlaz\Project\TaskExecution;
 
+use Attlaz\Client;
+use Attlaz\Project\App\Environment;
 use Attlaz\Project\Command\CommandManager;
 use Attlaz\Project\Model\TaskExecutionRequest;
 use Attlaz\Project\Model\TaskExecutionResult;
@@ -12,23 +14,43 @@ use Psr\Log\LoggerInterface;
 class AbstractTaskHandler
 {
     protected $commandManager;
+    protected $client;
+    protected $environment;
     protected $logger;
 
-    public function __construct(CommandManager $commandManager, LoggerInterface $logger)
-    {
+    public function __construct(
+        CommandManager $commandManager,
+        Client $client,
+        Environment $environment,
+        LoggerInterface $logger
+    ) {
         $this->commandManager = $commandManager;
+        $this->client = $client;
+        $this->environment = $environment;
         $this->logger = $logger;
     }
 
     public function execute(TaskExecutionRequest $taskExecutionRequest): int
     {
+        if ($this->environment->getProjectEnvironment()->isLocal) {
+            $this->client->updateTaskExecution($taskExecutionRequest->getExecutionId(), 'Running');
+        }
+
         $taskExecutionResult = $this->commandManager->executeTask($taskExecutionRequest);
 
         $this->sendResponse($taskExecutionResult);
 
         if ($taskExecutionResult->getSuccess()) {
+            if ($this->environment->getProjectEnvironment()->isLocal) {
+                $this->client->updateTaskExecution($taskExecutionRequest->getExecutionId(), 'Complete');
+            }
+
             return 0;
         } else {
+            if ($this->environment->getProjectEnvironment()->isLocal) {
+                $this->client->updateTaskExecution($taskExecutionRequest->getExecutionId(), 'Failed');
+            }
+
             //TODO: change exit code based on exception type
             return 1;
         }
