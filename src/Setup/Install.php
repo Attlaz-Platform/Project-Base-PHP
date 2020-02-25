b187b6687ab8c76a0066fd65b1d26358e718dd59
Echron <stijn@echron.com>
2020-02-25 12:30:21 +0000

    Setup: create bin directory if it doesn't exist yet

--- src/Setup/Install.php
+++ src/Setup/Install.php
@@ -20,42 +20,17 @@ class Install
 
     private static function copyBinFiles(): void
     {
+        // TODO: how to make sure the bin directory is not in git without modifiying the .gitignore file to avoid bad
+        // changes to that file
         $sourceBinDirectoryPath = realpath(dirname(__FILE__)) . \DIRECTORY_SEPARATOR . 'bin';
 
         $destinationBinDirectoryPath = FileSystem::joinPath(dirname(__FILE__), '..', '..', '..', '..', '..', 'bin');
         $destinationBinDirectoryPath = realpath($destinationBinDirectoryPath);
 
-        if (!\is_string($destinationBinDirectoryPath)) {
-            throw new \Exception('Destination directory not found');
+        if (!\is_string($destinationBinDirectoryPath) || !FileSystem::dirExists($destinationBinDirectoryPath)) {
+            FileSystem::createDir($destinationBinDirectoryPath, true);
         }
 
-        self::copyDirectory($sourceBinDirectoryPath, $destinationBinDirectoryPath);
-    }
-
-    private static function copyDirectory(string $src, string $dst): void
-    {
-        // open the source directory
-        $dir = opendir($src);
-        if ($dir === false) {
-            throw new \Exception('Unable to read source directory');
-        }
-
-        // Make the destination directory if not exist
-        @mkdir($dst);
-
-        // Loop through the files in source directory
-        while (false !== ($file = readdir($dir))) {
-            if (($file != '.') && ($file != '..')) {
-                if (is_dir($src . '/' . $file)) {
-                    // Recursively calling custom copy function
-                    // for sub directory
-                    self::copyDirectory($src . '/' . $file, $dst . '/' . $file);
-                } else {
-                    copy($src . '/' . $file, $dst . '/' . $file);
-                }
-            }
-        }
-
-        closedir($dir);
+        FileSystem::copyDirectory($sourceBinDirectoryPath, $destinationBinDirectoryPath, true);
     }
 }
