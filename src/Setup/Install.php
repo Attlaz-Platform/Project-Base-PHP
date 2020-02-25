735120e84c6579a4f19c2402b7a411891d028a0b
Echron <stijn@echron.com>
2020-02-25 13:07:04 +0000

    Setup: fix issue + make bash files executable

--- src/Setup/Install.php
+++ src/Setup/Install.php
@@ -13,24 +13,48 @@ class Install
         echo 'Setup project';
         try {
             self::copyBinFiles();
+            self::makeExecutable();
         } catch (\Throwable $ex) {
             throw new \Exception('Unable to run setup: ' . $ex->getMessage());
         }
+    }
+
+    private static function getVendorBinPath(): string
+    {
+        return realpath(dirname(__FILE__)) . \DIRECTORY_SEPARATOR . 'bin';
+    }
+
+    private static function getProjectBinPath(): string
+    {
+        return FileSystem::joinPath(dirname(__FILE__), '..', '..', '..', '..', '..', 'bin');
     }
 
     private static function copyBinFiles(): void
     {
         // TODO: how to make sure the bin directory is not in git without modifiying the .gitignore file to avoid bad
         // changes to that file
-        $sourceBinDirectoryPath = realpath(dirname(__FILE__)) . \DIRECTORY_SEPARATOR . 'bin';
+        $sourceBinDirectoryPath = self::getVendorBinPath();
+        $destinationBinDirectoryPath = self::getProjectBinPath();
 
-        $destinationBinDirectoryPath = FileSystem::joinPath(dirname(__FILE__), '..', '..', '..', '..', '..', 'bin');
-        $destinationBinDirectoryPath = realpath($destinationBinDirectoryPath);
-
-        if (!\is_string($destinationBinDirectoryPath) || !FileSystem::dirExists($destinationBinDirectoryPath)) {
+        if (!FileSystem::dirExists($destinationBinDirectoryPath)) {
             FileSystem::createDir($destinationBinDirectoryPath, true);
         }
 
         FileSystem::copyDirectory($sourceBinDirectoryPath, $destinationBinDirectoryPath, true);
+    }
+
+    private static function makeExecutable(): void
+    {
+        $sourceBinDirectoryPath = self::getVendorBinPath();
+        $destinationBinDirectoryPath = self::getProjectBinPath();
+
+        $files = [
+            $sourceBinDirectoryPath . \DIRECTORY_SEPARATOR . 'console.sh',
+            $destinationBinDirectoryPath . \DIRECTORY_SEPARATOR . 'console.sh',
+            $destinationBinDirectoryPath . \DIRECTORY_SEPARATOR . 'console',
+        ];
+        foreach ($files as $file) {
+            \chmod($sourceBinDirectoryPath, 0755);
+        }
     }
 }
