070013a4b95e8890e576561f02c6e8a5ec2575a3
Echron <stijn@echron.com>
2020-07-30 12:43:28 +0200

    Bin: bump docker worker version to 1.4.6

--- src/bin/windows/console.bat
+++ src/bin/windows/console.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-docker run --rm -it --init -v %cd%:/var/attlaz -w /var/attlaz hq.attlaz.com:2498/attlaz_worker:1.3.4 php bin/console %*
+docker run --rm -it --init -v %cd%:/var/attlaz -w /var/attlaz hq.attlaz.com:2498/attlaz_worker:1.4.6 php bin/console %*
