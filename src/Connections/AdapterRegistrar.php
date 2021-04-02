d0d9cabb2c4f03ed425a3dd6474fde9b2d8a0f21
Echron <stijn@echron.com>
2021-04-02 17:36:23 +0100

    Improve adapter connection loading

--- src/Connections/AdapterRegistrar.php
+++ src/Connections/AdapterRegistrar.php
@@ -9,25 +9,36 @@ class AdapterRegistrar
 {
     private static $adapters = [];
 
-    public static function register(string $adapterName, string $path): void
+    public static function register(string $adapterName, string $path, string $factoryClassName): void
     {
-        die('register ok');
-        if (isset(self::$adapters[$adapterName])) {
-            throw new \LogicException(
-                $adapterName . '\' from \'' . $path . '\' '
-                . 'has been already defined in \'' . self::$adapters[$adapterName] . '\'.'
+        $formattedAdapterName = self::formatAdapterName($adapterName);
+        if (isset(self::$adapters[$formattedAdapterName])) {
+            throw new \LogicException($adapterName . '\' from \'' . $path . '\' '
+                . 'has been already defined in \'' . self::$adapters[$formattedAdapterName] . '\'.'
             );
         } else {
-            self::$adapters[$adapterName] = str_replace('\\', '/', $path);
+            self::$adapters[$formattedAdapterName] = ['path' => str_replace('\\', '/', $path), 'factory' => $factoryClassName];
         }
     }
 
-    public static function getPath(string $adapterName): ?string
+    public static function getFactoryClassName(string $adapterName): ?string
     {
-        if (isset(self::$adapters[$adapterName])) {
-            return self::$adapters[$adapterName];
+        // TODO: class should be instance of AdapterFactory
+        $formattedAdapterName = self::formatAdapterName($adapterName);
+        if (isset(self::$adapters[$formattedAdapterName])) {
+            return self::$adapters[$formattedAdapterName]['factory'];
         }
         return null;
+    }
+
+    public static function getAdapterNames(): array
+    {
+        return \array_keys(self::$adapters);
+    }
+
+    private static function formatAdapterName(string $adapterName): string
+    {
+        return \strtolower($adapterName);
     }
 
 }
