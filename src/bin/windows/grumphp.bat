a98b3f5b3839dcfc63d43e8ab6547a76f8a5d3c6
Stijn Duynslaeger <stijn@echron.com>
2022-12-01 16:26:55 +0000

    Console: update images to PHP 8.1

--- src/bin/windows/grumphp.bat
+++ src/bin/windows/grumphp.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-docker run --rm -it -w /var/attlaz -v %cd%:/var/attlaz hq.attlaz.com:2498/php7_4:1.1.0 php vendor/bin/grumphp run
+docker run --rm -it -w /var/attlaz -v %cd%:/var/attlaz attlaz/php:8.1 php vendor/bin/grumphp run
