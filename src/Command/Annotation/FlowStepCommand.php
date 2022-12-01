6225fd00e20e09110ce17e71343b5ca13c56a1e2
Stijn Duynslaeger <stijn@echron.com>
2022-12-01 17:24:56 +0000

    Command discovery: use attributes for command definition if available

---
+++ src/Command/Annotation/FlowStepCommand.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace Attlaz\Project\Command\Annotation;
+
+
+/**
+ * FlowStepCommand
+ *
+ * Marks a class as a step flow command
+ *
+ * @author Stijn Duynslaeger <stijn@attlaz.com>
+ */
+#[\Attribute(\Attribute::TARGET_CLASS)] final class FlowStepCommand
+{
+
+    /**
+     * Id of the flow step
+     */
+    public string $flowStepId;
+    public array $connections;
+    
+    public function __construct(string $flowStepId, array $connections = [])
+    {
+        $this->flowStepId = $flowStepId;
+        $this->connections = $connections;
+    }
+
+    public function getFlowStepId(): string
+    {
+        return $this->flowStepId;
+    }
+}
