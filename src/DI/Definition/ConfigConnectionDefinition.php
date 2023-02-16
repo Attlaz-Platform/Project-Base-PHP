c51c8337789c4121c991a23df6c248c2bee66088
Stijn Duynslaeger <stijn@echron.com>
2023-02-16 16:23:06 +0000

    Add config connection definition

---
+++ src/DI/Definition/ConfigConnectionDefinition.php
@@ -0,0 +1,78 @@
+<?php
+declare(strict_types=1);
+
+namespace Attlaz\Project\DI\Definition;
+
+use Attlaz\Project\App\Config;
+use Attlaz\Project\Connections\ConnectionPool;
+use DI\Definition\Definition;
+use DI\Definition\SelfResolvingDefinition;
+use Psr\Container\ContainerInterface;
+
+/**
+ * Use this when the connection definition is defined in configuration. Pass along the configuration key containing the connection id
+ */
+class ConfigConnectionDefinition implements Definition, SelfResolvingDefinition
+{
+
+    private string $name = '';
+
+    private string $configKey;
+
+    public function __construct(string $configKey)
+    {
+        $this->configKey = $configKey;
+
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+
+    public function resolve(ContainerInterface $container): mixed
+    {
+        $config = $container->get(Config::class);
+
+        $connectionIdentifier = $config->get($this->configKey, 'string');
+
+        /** @var ConnectionPool $connectionPool */
+        $connectionPool = $container->get(ConnectionPool::class);
+
+        return $connectionPool->getConnection($connectionIdentifier);
+
+    }
+
+    public function isResolvable(ContainerInterface $container): bool
+    {
+        /** @var Config $config */
+        $config = $container->get(Config::class);
+
+        $connectionIdentifier = $config->get($this->configKey, 'string');
+        if ($connectionIdentifier === null) {
+            return false;
+        }
+
+        /** @var ConnectionPool $connectionPool */
+        $connectionPool = $container->get(ConnectionPool::class);
+
+        $keys = $connectionPool->getConnectionDefinitionKeys();
+        return \array_key_exists($connectionIdentifier, $keys);
+    }
+
+    public function replaceNestedDefinitions(callable $replacer): void
+    {
+        // no nested definitions
+    }
+
+    public function __toString(): string
+    {
+        return 'Config Connection: ' . $this->configKey;
+    }
+}
