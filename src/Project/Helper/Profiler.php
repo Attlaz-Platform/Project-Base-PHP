cbffd55535d3e1e99d3e1a486df26bfe6eac491b
Stijn Duynslaeger <stijn@echron.com>
2024-11-05 14:58:58 +0000

    Project: improve profiling

--- src/Project/Helper/Profiler.php
+++ src/Project/Helper/Profiler.php
@@ -12,14 +12,26 @@ class Profiler
     /**
      * @var LoggerInterface
      */
-    private LoggerInterface $logger;
+    private LoggerInterface|null $logger = null;
 
     private array $profiles = [];
 
+    private array|null $currentProfile = null;
 
-    public function __construct(LoggerInterface $logger)
+
+    public function __construct()
     {
-        $this->logger = $logger;
+
+    }
+
+    public function start(string $key, string $label = ''): void
+    {
+        $this->startProfile($key, $label);
+    }
+
+    public function finish(string $key, string $label = ''): void
+    {
+        $this->endProfile($key);
     }
 
     public function startProfile(string $key, string $label = ''): void
@@ -27,28 +39,57 @@ class Profiler
         if (empty($label)) {
             $label = $key;
         }
-        if (isset($this->profiles[$key])) {
-            $this->logger->warning('Unable to start profile `' . $key . '`: already started');
+        $fullKey = $this->currentProfile === null ? $key : $this->currentProfile['path'] . '::' . $key;
+
+        if (isset($this->profiles[$fullKey])) {
+            $this->logger->warning('Unable to start profile `' . $fullKey . '`: already started');
         }
-        $this->profiles[$key] = [
-            'key'   => $key,
+        $profile = [
+            'path' => $this->currentProfile === null ? '' : $this->currentProfile['path'] . '::' . $this->currentProfile['key'],
+
+            'key' => $key,
             'label' => $label,
             'start' => microtime(true),
-            'end'   => null,
+            'end' => null,
+            'ellapse' => null,
+            'elapse readable' => null,
         ];
+        $this->profiles[$profile['path'] . '::' . $key] = $profile;
+        $this->currentProfile = $profile;
     }
 
     public function endProfile(string $key): void
     {
         $end = microtime(true);
-        if (!isset($this->profiles[$key])) {
-            $this->logger->warning('Unable to end profile `' . $key . '`: not found (make sure it is started)');
-        }
-        $this->profiles[$key]['end'] = $end;
 
-        $elapsedSeconds = $this->profiles[$key]['end'] - $this->profiles[$key]['start'];
-        $this->profiles[$key]['elapse'] = $elapsedSeconds;
-        $this->profiles[$key]['elapse readable'] = Time::readableSeconds($elapsedSeconds);
+        // Shouldn't this always be the case?
+        if ($key === $this->currentProfile['key']) {
+            $fullKey = $this->currentProfile['path'] . '::' . $key;
+        } else {
+            echo 'Wrong' . PHP_EOL;
+            $fullKey = $this->currentProfile === null ? $key : $this->currentProfile['path'] . '::' . $key;
+        }
+
+        if (!isset($this->profiles[$fullKey])) {
+
+            echo 'Unable to find ' . $fullKey . PHP_EOL;
+            var_dump(array_keys($this->profiles));
+            //$this->logger->warning('Unable to end profile `' . $key . '`: not found (make sure it is started)');
+            return;
+        }
+        $profile = $this->profiles[$fullKey];
+        $profile['end'] = $end;
+
+        $elapsedSeconds = $profile['end'] - $profile['start'];
+        $profile['elapse'] = $elapsedSeconds;
+        $profile['elapse readable'] = Time::readableSeconds($elapsedSeconds);
+
+        $this->profiles[$fullKey] = $profile;
+
+        $x = $this->currentProfile['path'] === '' ? null : $this->currentProfile['path'];
+
+        $currentProfileParent = $x === null ? null : $this->profiles[$x];
+        $this->currentProfile = $currentProfileParent;
     }
 
     public function getProfile(string $key): array
@@ -59,4 +100,18 @@ class Profiler
         return $this->profiles[$key];
     }
 
+    public function getProfiles(): array
+    {
+        return $this->profiles;
+    }
+
+    public function debug(): array
+    {
+        $profiles = $this->profiles;
+        $output = [];
+        foreach ($profiles as $k => $x) {
+            $output[] = str_repeat('    ', count(explode('::', $x['path']))) . ' ' . $x['label'] . ' ' . $x['elapse readable'];
+        }
+        return $output;
+    }
 }
