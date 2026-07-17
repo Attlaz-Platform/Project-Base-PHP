3bc850b3b76c57536a2b43b7b52bd8d64785f2c4
Stijn Duynslaeger <stijn@echron.com>
2026-07-17 11:29:01 +0200

    Add parentheses around console run (bugfix)

--- src/Project/Setup/bin/console.php
+++ src/Project/Setup/bin/console.php
@@ -4,4 +4,4 @@ declare(strict_types=1);
 // Fast in-container / worker entry point: boot the app directly. No version checks, no Docker.
 // The Attlaz worker calls this, and bin/console runs it inside the container.
 require __DIR__ . '/../vendor/autoload.php';
-new \Attlaz\Project\Project(\dirname(__DIR__))->run();
+(new \Attlaz\Project\Project(\dirname(__DIR__)))->run();
